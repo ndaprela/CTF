7e9d75f9020658d5aa0cbe185bbea058c90731ea
nico <25092016+ndaprela@users.noreply.github.com>
2020-07-20 11:21:06 +0200

    Update solve.py

--- 2020/rgbCTF/A Basic Challenge/solve.py
+++ 2020/rgbCTF/A Basic Challenge/solve.py
@@ -3,6 +3,5 @@ import base64
 with open("basic_chall.txt") as f:
 	tokens = f.read().split()
 
-print("".join(list(map(lambda t: chr(int(t, 8)), base64.b64decode(("".join(list(map(lambda h: chr(int(h, 16)), "".join(list(map(lambda t: chr(t), map(lambda t: int(t, 2), tokens)))).split()))).encode("utf-8"))).split()))))
-
+print("".join(map(lambda t: chr(int(t, 8)), base64.b64decode(("".join(map(lambda h: chr(int(h, 16)), "".join(map(lambda t: chr(t), map(lambda t: int(t, 2), tokens))).split())).encode("utf-8"))).split())))
 # rgbCTF{c0ngr4ts_0n_b3ing_B4SIC}
